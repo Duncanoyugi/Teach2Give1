cefb9c62babb553bc3ae3787fae9c0655944e31c
DuncanOyugi <duncanoyugi2001@gmail.com>
2025-05-06 16:00:39 +0300

    Added event listeners

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+// A simple demonstration of a basic Node.js server
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
